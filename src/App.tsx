d4e59ea1985c5972d43c8caa0e4b5ad5b5e13c7f
yeonsuBaek <soothbugger@gmail.com>
2024-03-20 19:33:55 +0900

    ✨ 404 페이지 생성 및 연결

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/feature/Header'
 import Main from './pages/Main'
 import Pokemon from './pages/Pokemon'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />}></Route>
           <Route path="/pokemon/:id" element={<Pokemon />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </main>
     </BrowserRouter>
