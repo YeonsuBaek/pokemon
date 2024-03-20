5341e2324be4c1a246d2de56fa3482cb743e16ff
yeonsuBaek <soothbugger@gmail.com>
2024-03-20 19:46:29 +0900

    🚧 Search 컴포넌트 추가 (스타일 수정 필요)

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import PokemonList from '../components/feature/Main/PokemonList'
+import { TextField } from '@yeonsubaek/yeonsui'
 
 const Main = () => {
   return (
     <div>
+      <TextField icon="Filter" placeholder="번호를 입력해 포켓몬을 찾아보세요." />
       <PokemonList />
     </div>
   )
