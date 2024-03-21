15f7e613063b7142418bfdabbe7b862ff6310672
yeonsuBaek <soothbugger@gmail.com>
2024-03-22 08:38:53 +0900

    🚧 Pokemons 데이터 패칭

--- src/components/feature/Main/PokemonList.tsx
+++ src/components/feature/Main/PokemonList.tsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PokemonItem from './PokemonItem'
 
+type PokemonType = {
+  id: number
+  name: string
+  url?: string
+}
+
+const LIMIT = 30
+
 const PokemonList = () => {
+  const [pokemons, setPokemons] = useState<PokemonType[]>([])
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}&offset=0`)
+        const data = await response.json()
+        const pokemonsData = data.results.map((pokemon: { name: string; url: string }, index: number) => {
+          return {
+            ...pokemon,
+            id: index + 1,
+          }
+        })
+        setPokemons(pokemonsData)
+      } catch (error) {
+        console.error('Error fetching Pokemon:', error)
+      }
+    }
+
+    fetchPokemons()
+  }, [])
+
   return (
     <ul className="flex flex-wrap justify-center gap-4">
-      <PokemonItem />
-      <PokemonItem />
-      <PokemonItem />
+      {pokemons.map((pokemon: PokemonType) => (
+        <PokemonItem />
+      ))}
     </ul>
   )
 }
