b57c993e8fe9464efc371a2ae23ed3b87868f55b
yeonsuBaek <soothbugger@gmail.com>
2024-03-22 10:42:27 +0900

    ♻️ PokemonCard 컴포넌트로 리팩터링

--- src/components/feature/Pokemon/EvolutionItem.tsx
+++ src/components/feature/Pokemon/EvolutionItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import PokemonCard from '../../block/PokemonCard'
 
 interface EvolutionItemProps {
   name: string
@@ -11,17 +12,7 @@ const EvolutionItem = ({ name, id, hasNext }: EvolutionItemProps) => {
   return (
     <li key={id} className={hasNext ? "after:content-['▶️'] after:mx-4 flex items-center" : ''}>
       <Link to={`/pokemon/${id}`}>
-        <figure className="flex flex-col items-center">
-          <img
-            className="mb-2 w-[100px] h-[100px]"
-            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
-            alt="Name"
-          />
-          <figcaption className="flex flex-col items-center">
-            <span className="text-gray-500">No.{id}</span>
-            <h2>{name}</h2>
-          </figcaption>
-        </figure>
+        <PokemonCard name={name} id={id} size="small" />
       </Link>
     </li>
   )
--- src/pages/Pokemon.tsx
+++ src/pages/Pokemon.tsx
@@ -2,6 +2,7 @@ import { Button } from '@yeonsubaek/yeonsui'
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import EvolutionsList from '../components/feature/Pokemon/EvolutionsList'
+import PokemonCard from '../components/block/PokemonCard'
 
 type typeOfInfoType = {
   slot: number
@@ -22,8 +23,6 @@ const Pokemon = () => {
   const { id } = useParams()
   const [information, setInformation] = useState<informationType>()
   const [evolutionUrl, setEvolutionUrl] = useState('')
-
-  const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
 
   useEffect(() => {
     const fetchPokemon = async () => {
@@ -46,13 +45,7 @@ const Pokemon = () => {
     <div className="flex flex-col items-center mb-8">
       {information ? (
         <>
-          <figure className="mb-4">
-            <img className="mb-4 w-full aspect-square max-w-[400px]" src={imageUrl} alt={information.name} />
-            <figcaption className="flex flex-col items-center">
-              <span className="text-gray-500">No.{id}</span>
-              <h2 className="text-2xl font-bold">{information.name}</h2>
-            </figcaption>
-          </figure>
+          <PokemonCard name={information.name} id={Number(id)} />
           <dl className="flex justify-between mb-4 px-5 py-4 border border-gray-300 rounded w-full max-w-[400px]">
             <div className="flex items-start justify-start gap-2">
               <dt className="text-gray-500">타입</dt>
