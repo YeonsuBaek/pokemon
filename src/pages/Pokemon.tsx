8ac5a6aa924c8b159507b7e15139dacbe65acef0
yeonsuBaek <soothbugger@gmail.com>
2024-03-22 10:23:24 +0900

    🚧 Pokemon evolution 데이터 연결

--- src/pages/Pokemon.tsx
+++ src/pages/Pokemon.tsx
@@ -10,33 +10,66 @@ type typeOfInfoType = {
   }
 }
 
-interface informationType {
+type informationType = {
   height: number
   name: string
   types: typeOfInfoType[]
   weight: number
 }
 
+type evolutionType = {
+  name: string
+  id: number
+}
+
 const Pokemon = () => {
   const { id } = useParams()
   const [information, setInformation] = useState<informationType>()
+  const [evolutionLevel, setEvolutionLevel] = useState<evolutionType[]>([])
 
   const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
 
   useEffect(() => {
+    const fetchEvolution = async (url: string) => {
+      try {
+        const evolution = await fetch(url)
+        const evolutionData = await evolution.json()
+        const newLevel = []
+        let elem = evolutionData.chain
+        while (elem) {
+          if (elem.species?.name && elem.species?.url) {
+            const { name, url: pokemonUrl } = elem.species
+            const id = Number(
+              pokemonUrl
+                .split('/')
+                .filter((part: string) => !!part)
+                .pop()
+            )
+            newLevel.push({ name, id })
+          }
+          elem = elem.evolves_to[0]
+        }
+        setEvolutionLevel(newLevel)
+      } catch (error) {
+        console.error('Error fetching Evolution:', error)
+      }
+    }
+
     const fetchPokemon = async () => {
       try {
         const information = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        const species = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`)
         const informationData = await information.json()
+        const speciesData = await species.json()
         setInformation(informationData)
-        console.log(informationData)
+        await fetchEvolution(speciesData.evolution_chain.url)
       } catch (error) {
         console.error('Error fetching Pokemon:', error)
       }
     }
 
     fetchPokemon()
-  }, [])
+  }, [id])
 
   return (
     <div className="flex flex-col items-center mb-8">
@@ -68,33 +101,28 @@ const Pokemon = () => {
             </div>
           </dl>
           <ul className="flex mb-4">
-            <li className="after:content-['▶️'] after:mx-4 flex items-center">
-              <figure className="flex flex-col items-center">
-                <img className="mb-2 w-[100px] h-[100px]" src="http://placehold.it/100x100" alt="Name" />
-                <figcaption className="flex flex-col items-center">
-                  <span className="text-gray-500">No.1</span>
-                  <h2>Name</h2>
-                </figcaption>
-              </figure>
-            </li>
-            <li className="after:content-['▶️'] after:mx-4 flex items-center">
-              <figure className="flex flex-col items-center">
-                <img className="mb-2 w-[100px] h-[100px]" src="http://placehold.it/100x100" alt="Name" />
-                <figcaption className="flex flex-col items-center">
-                  <span className="text-gray-500">No.1</span>
-                  <h2>Name</h2>
-                </figcaption>
-              </figure>
-            </li>
-            <li>
-              <figure className="flex flex-col items-center">
-                <img className="mb-2 w-[100px] h-[100px]" src="http://placehold.it/100x100" alt="Name" />
-                <figcaption className="flex flex-col items-center">
-                  <span className="text-gray-500">No.1</span>
-                  <h2>Name</h2>
-                </figcaption>
-              </figure>
-            </li>
+            {evolutionLevel.map(({ name, id }, idx) => {
+              return (
+                <li
+                  key={id}
+                  className={idx < evolutionLevel.length - 1 ? "after:content-['▶️'] after:mx-4 flex items-center" : ''}
+                >
+                  <Link to={`/pokemon/${id}`}>
+                    <figure className="flex flex-col items-center">
+                      <img
+                        className="mb-2 w-[100px] h-[100px]"
+                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
+                        alt="Name"
+                      />
+                      <figcaption className="flex flex-col items-center">
+                        <span className="text-gray-500">No.{id}</span>
+                        <h2>{name}</h2>
+                      </figcaption>
+                    </figure>
+                  </Link>
+                </li>
+              )
+            })}
           </ul>
         </>
       ) : null}
