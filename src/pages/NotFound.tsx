162e12bdf07da1092036709ba6e453f3fc537f77
yeonsuBaek <soothbugger@gmail.com>
2024-03-24 14:07:20 +0900

    ⚡️ 404페이지 SEO 최적화 (71점 -> 90점)

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,16 +1,24 @@
 import { Button } from '@yeonsubaek/yeonsui'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import MetaTag from '../components/block/MetaTag'
 
 const NotFound = () => {
   return (
-    <div className="flex-col h-[80vh] flex-center">
-      <h2 className="text-2xl font-bold">404 ERROR</h2>
-      <p className="mb-10">죄송합니다. 원하시는 페이지를 찾을 수 없습니다.</p>
-      <Link to="/">
-        <Button>돌아가기</Button>
-      </Link>
-    </div>
+    <>
+      <MetaTag
+        title="포켓몬 도감 사이트 | 접근할 수 없는 페이지"
+        description="죄송합니다. 원하시는 페이지를 찾을 수 없습니다."
+        keywords="Pokemons, 포켓몬, 포켓몬 도감"
+      />
+      <div className="flex-col h-[80vh] flex-center">
+        <h2 className="text-2xl font-bold">404 ERROR</h2>
+        <p className="mb-10">죄송합니다. 원하시는 페이지를 찾을 수 없습니다.</p>
+        <Link to="/">
+          <Button>돌아가기</Button>
+        </Link>
+      </div>
+    </>
   )
 }
 
