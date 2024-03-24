0e7bec983173f50e68e576e3139ad54c37cf4718
yeonsuBaek <soothbugger@gmail.com>
2024-03-24 13:52:05 +0900

    ⚡️ SEO 최적화 (82점 -> 92점)

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import Header from './components/feature/Header'
 import Main from './pages/Main'
 import Pokemon from './pages/Pokemon'
 import NotFound from './pages/NotFound'
+import { HelmetProvider } from 'react-helmet-async'
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <main className="w-full mt-4 px-4 sm:px-10 lg:max-w-[1000px] md:mx-auto lg:p-0   ">
-        <Routes>
-          <Route path="/" element={<Main />}></Route>
-          <Route path="/pokemon/:id" element={<Pokemon />}></Route>
-          <Route path="*" element={<NotFound />}></Route>
-        </Routes>
-      </main>
+      <HelmetProvider>
+        <Header />
+        <main className="w-full mt-4 px-4 sm:px-10 lg:max-w-[1000px] md:mx-auto lg:px-0">
+          <Routes>
+            <Route path="/" element={<Main />}></Route>
+            <Route path="/pokemon/:id" element={<Pokemon />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
+        </main>
+      </HelmetProvider>
     </BrowserRouter>
   )
 }
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,15 +1,21 @@
 import React, { useState } from 'react'
 import PokemonList from '../components/feature/Main/PokemonList'
 import Search from '../components/feature/Main/Search'
+import { Helmet } from 'react-helmet-async'
 
 const Main = () => {
   const [searchValue, setSearchValue] = useState('')
 
   return (
-    <div>
+    <>
+      <Helmet>
+        <title>포켓몬 도감 사이트</title>
+        <meta name="description" content="포켓몬 종류를 한 눈에 알아봅시다." />
+        <meta name="keywords" content="Pokemons, 포켓몬, 포켓몬 도감" />
+      </Helmet>
       <Search onChangeSearch={setSearchValue} />
       <PokemonList searchValue={searchValue} />
-    </div>
+    </>
   )
 }
 
