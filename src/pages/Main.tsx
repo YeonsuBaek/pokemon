720132d1b99676385ad385d9cef6af86c2fbd562
yeonsuBaek <soothbugger@gmail.com>
2024-03-24 14:01:09 +0900

    ♻️ MetaTag 컴포넌트 생성

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react'
 import PokemonList from '../components/feature/Main/PokemonList'
 import Search from '../components/feature/Main/Search'
-import { Helmet } from 'react-helmet-async'
+import MetaTag from '../components/block/MetaTag'
 
 const Main = () => {
   const [searchValue, setSearchValue] = useState('')
 
   return (
     <>
-      <Helmet>
-        <title>포켓몬 도감 사이트</title>
-        <meta name="description" content="포켓몬 종류를 한 눈에 알아봅시다." />
-        <meta name="keywords" content="Pokemons, 포켓몬, 포켓몬 도감" />
-      </Helmet>
+      <MetaTag
+        title="포켓몬 도감 사이트"
+        description="포켓몬 종류를 한 눈에 알아봅시다."
+        keywords="Pokemons, 포켓몬, 포켓몬 도감"
+      />
       <Search onChangeSearch={setSearchValue} />
       <PokemonList searchValue={searchValue} />
     </>
